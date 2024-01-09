a06c71f8bb3c5d9aa7f2bdf5eb03774db6276946
SuhaibQudah <suhaib.qudah@rhinosoft.io>
2024-01-09 13:58:08 +0300

    handle some bugs in dist and vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/JsonToCSV.vue',
+      name: 'JsonToCSV',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
